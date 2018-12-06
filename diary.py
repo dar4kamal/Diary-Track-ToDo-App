670072a6086ca8f98aa1b3af66594c885e9b0c4b
dar4kamal <sasaa9646@gmail.com>
2018-12-06 09:39:35 +0200

    added getdata + save to Json file

--- diary.py
+++ diary.py
@@ -1,9 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from datetime import datetime
 from PyQt5.uic import *
 from os import path
 from time import *
 import json
 import sys
+import os
 
 FORM_CLASS,_=loadUiType(path.join(path.dirname(__file__),"diaryApp.ui"))
 
@@ -13,8 +15,12 @@ class MainApp(QtWidgets.QMainWindow,FORM_CLASS):
         super(MainApp,self).__init__(parent)
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
+        self.globals()
         self.handle_ui()
-        self.handle_btns()
+        self.handle_btns()        
+
+    def globals(self):
+        self.fileName = "data.json"
 
     def handle_ui(self):
         QtWidgets.QMainWindow.setWindowTitle(self,"Diary App")
@@ -23,8 +29,30 @@ class MainApp(QtWidgets.QMainWindow,FORM_CLASS):
     def handle_btns(self):
         self.saveDiary.clicked.connect(self.save)
     
+    def getData(self):
+        try:
+            with open(self.fileName, mode='r') as f:
+                data = json.load(f)
+        except:
+            if os.path.exists(self.fileName):
+                os.remove(self.fileName)
+            with open(self.fileName, mode='w') as f:
+                json.dump([], f)
+            with open(self.fileName, mode='r') as f:
+                data = json.load(f)
+        return data
+
     def save(self):
-        pass
+                
+        data = self.getData()
+        output = {
+            "content":str(self.editDiary.document().toPlainText()),
+            "date":str(datetime.now().date()),
+            "time":str(strftime("%H:%M:%S"))
+        }
+        data.append(output)        
+        with open(self.fileName, mode='w') as f:
+            json.dump(data, f)
         
 
 def main():
